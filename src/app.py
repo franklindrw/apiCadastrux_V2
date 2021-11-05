7c5d4d1dfbbbff7ebb01568babad2f43f4953585
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-11-04 21:24:14 -0300

    adicionando Rotas de Usuarios e Produtos

--- src/app.py
+++ src/app.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 
 conexao = MySQL(app)
 
+# usuarios
 @app.route('/usuarios', methods=['GET'])
 def listarUsuarios():
     try:
         cursor = conexao.connection.cursor()
-        sql="SELECT IDUSUARIO, USUARIO, SENHA FROM USUARIOS"
+        sql="SELECT IDUSUARIO, USUARIO, STATUS FROM USUARIOS"
         cursor.execute(sql)
         dados = cursor.fetchall()
         usuarios = []
         for linha in dados:
-            usuario = {'id':linha[0], 'usuario':linha[1], 'senha':linha[2]}
+            usuario = {'id':linha[0], 'usuario':linha[1], 'status':linha[2]}
             usuarios.append(usuario)        
         return jsonify({'usuarios': usuarios, 'mesagem':"Usuários Listados"})
     except Exception as ex:
         return jsonify({'mesagem':"Nenhuma informação encontrada"})
-
 
 @app.route('/usuarios/<usuario>/<senha>', methods=['GET'])
 def verificarUsuario(usuario, senha):
@@ -36,6 +36,22 @@ def verificarUsuario(usuario, senha):
             return jsonify({})
     except Exception as ex:
         return jsonify({'mensagem':"Erro ao consultar na API"})
+
+# Produtos
+@app.route('/outdoor', methods=['GET'])
+def listarProdutos():
+    try:
+        cursor = conexao.connection.cursor()
+        sql="SELECT IDPRODUTO, PRODNOME, DESCRICAO, IMAGEM FROM PRODUTOS"
+        cursor.execute(sql)
+        dados = cursor.fetchall()
+        produtos = []
+        for linha in dados:
+            produto = {'id':linha[0], 'produto':linha[1], 'descricao':linha[2], 'imagem':linha[3]}
+            produtos.append(produto)        
+        return jsonify({'produtos': produtos})
+    except Exception as ex:
+        return jsonify({'mesagem':"Nenhuma informação encontrada"})
 
 def pagina_nao_encontrada(error):
     return "<h2>A página que está tentando buscar não existe</h2>"
